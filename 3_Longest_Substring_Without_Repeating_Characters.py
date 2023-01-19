64beb45c5d9bce517054822f60a7a99f68cfbb6c
json-0201 <ghks940201@ucla.edu>
2023-01-18 16:57:18 -0800

    Add files via upload

---
+++ 3_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        Returns the length of the longest substring without repeating characters.
+        String s consists of English letters, digits, symbols and spaces.
+        """
+
+        # trivial input
+        if not s:
+            return 0
+        if len(s) == 1:
+            return 1
+        
+        temp, words = [], []
+        index = 0
+
+        while s:
+            if index == len(s):
+                words.append("".join(temp))
+                break
+            elif s[index] in temp:
+                words.append("".join(temp))
+                temp = []
+                s = "".join([char for char in s][1:])
+                index = 0
+            temp.append(s[index])
+            index += 1
+            
+
+        return len(max(words, key=len))
+
+solution = Solution()
+print(solution.lengthOfLongestSubstring("abcabcbb"))
